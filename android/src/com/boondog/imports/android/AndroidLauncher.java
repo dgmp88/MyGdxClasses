dca31b7eb0a094fc6774f4bd27e0248de2ed1dad
dgmp88 <dgmprichard@gmail.com>
2015-01-05 02:01:07 +0000

    First commit

---
+++ android/src/com/boondog/imports/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.boondog.imports.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.boondog.imports.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
