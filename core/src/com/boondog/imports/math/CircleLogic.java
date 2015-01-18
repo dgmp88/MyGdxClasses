68dcedf8232157907934288f16f508f2e9e67b67
dgmp88 <dgmprichard@gmail.com>
2015-01-18 11:30:24 +0000

    Added "within arc" method

--- core/src/com/boondog/imports/math/CircleLogic.java
+++ core/src/com/boondog/imports/math/CircleLogic.java
@@ -6,7 +6,6 @@ public class CircleLogic {
 	static Vector2 v = new Vector2();
 	static float f;
 	
-	
 	public static Vector2 findPos(Vector2 center, float radius, float angle, float ellipse) {
 		// circular motion: ellipse == 0;
 		// straight motion: ellipse appriaching 0 or approaching Inf (Star s1 = new Star(solarSys, new Vector2(camX/2, 10), 180, 0.03f, 2f, 1000f);
@@ -18,6 +17,7 @@ public class CircleLogic {
 	
 	
 	public static boolean lineInArc(float arcDir, float arcWidth, float angle) {
+		// Uses the solution here: http://gamedev.stackexchange.com/questions/4467/comparing-angles-and-working-out-the-difference
 		f = 180 - Math.abs(Math.abs((arcDir % 360) - (angle % 360))-180);
 		return (f < arcWidth/2 && f > -arcWidth/2);
 	}
