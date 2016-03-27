fd40c0e5fc93b3f69d5c774ec71966fdddb9f6cc
dgmp88 <dgmprichard@gmail.com>
2016-03-27 16:53:27 +0100

    Smarter asset management to handle this weird new problem on desktop

--- core/src/com/boondog/imports/io/Assets.java
+++ core/src/com/boondog/imports/io/Assets.java
@@ -1,11 +1,12 @@
 package com.boondog.imports.io;
 
-
 import java.util.HashMap;
 
+import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -17,7 +18,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 public class Assets {
 	AssetManager manager = new AssetManager();
 	HashMap<String,BitmapFont> fonts = new HashMap<String, BitmapFont>();
-	private static String baseDir = "";
+	private static String baseDir = getBaseDir();
 	private static String atlasDir = "atlas/";
 	private static String fullAtlasDir =  baseDir + atlasDir;
 	
@@ -33,6 +34,16 @@ public class Assets {
 		}		
 	}
 	
+	private static String getBaseDir() {
+		// On desktop, sometimes everything goes wrong unless you use this directory as your head
+		// http://stackoverflow.com/questions/12681678/libgdx-how-to-get-a-list-of-files-in-a-directory
+		String baseDir = "";
+		if (Gdx.app.getType() == ApplicationType.Desktop) {
+			baseDir = "./bin/";
+		}
+		return baseDir;
+	}
+
 	public void clearAtlas(String atlas) {
 		manager.unload(fullAtlasDir + atlas + ".atlas");
 	}
@@ -109,7 +120,7 @@ public class Assets {
 		fullAtlasDir = baseDir + atlasDir;
 	}
 	
-	public static void setBaseDir(String string) {
-		baseDir = string;
+	public static FileHandle[] listFilesInDir(String dir) {
+		return new FileHandle(baseDir + dir).list();
 	}
 }
